5fdac796c607671311fa9fb260522543f1a349f6
Makoto Nihei <nihemak@gmail.com>
2018-09-13 05:23:02 +0900

    Add aws_dynamodb_table.todos

--- modules/dynamodb/main.tf
+++ modules/dynamodb/main.tf
@@ -30,3 +30,15 @@ resource "aws_dynamodb_table" "hoges" {
     projection_type    = "ALL"
   }
 }
+
+resource "aws_dynamodb_table" "todos" {
+  name           = "${var.resource_prefix}-todos"
+  read_capacity  = 1
+  write_capacity = 1
+  hash_key       = "id"
+
+  attribute {
+    name = "id"
+    type = "S"
+  }
+}
