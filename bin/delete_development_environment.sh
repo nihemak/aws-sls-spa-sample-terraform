70389938514770b05bcbe5815f46886d600b37c4
Makoto Nihei <nihemak@gmail.com>
2018-09-30 13:08:52 +0900

    Add log to delete_development_environment

--- bin/delete_development_environment.sh
+++ bin/delete_development_environment.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
-## info
+##
+echo "START: info..."
+##
 
 cd environments/service/web/development/ || exit 99
 
 terraform init -backend-config="bucket=${TF_VAR_s3_bucket_terraform_state_id:?}" \
                -backend-config="key=${TF_VAR_tfstate_service_web_key:?}"
+
 codebuild_destroy_web_name=$(terraform output codebuild_destroy_web_name)
+echo "codebuild_destroy_web_name: ${codebuild_destroy_web_name}"
 
 cd - || exit 99
 
@@ -14,7 +18,9 @@ cd environments/service/api/development/ || exit 99
 
 terraform init -backend-config="bucket=${TF_VAR_s3_bucket_terraform_state_id}" \
                -backend-config="key=${TF_VAR_tfstate_service_api_key:?}"
+
 codebuild_destroy_api_name=$(terraform output codebuild_destroy_api_name)
+echo "codebuild_destroy_api_name: ${codebuild_destroy_api_name}"
 
 cd - || exit 99
 
@@ -22,12 +28,16 @@ cd environments/service/base/pre/ || exit 99
 
 terraform init -backend-config="bucket=${TF_VAR_s3_bucket_terraform_state_id}" \
                -backend-config="key=${TF_VAR_tfstate_service_base_pre_key:?}"
+
 cloudformation_api_stack=$(terraform output cloudformation_api_stack)
+echo "cloudformation_api_stack: ${cloudformation_api_stack}"
+
 TF_VAR_apigw_api_id=$( \
     aws cloudformation describe-stack-resource --stack-name "${cloudformation_api_stack}" \
                                                --logical-resource-id ApiGatewayRestApi | \
     jq -r '.StackResourceDetail.PhysicalResourceId' \
 )
+echo "TF_VAR_apigw_api_id: ${TF_VAR_apigw_api_id}"
 export TF_VAR_apigw_api_id
 
 cd - || exit 99
@@ -36,13 +46,21 @@ cd environments/setup/development/ || exit 99
 
 terraform init -backend-config="bucket=${TF_VAR_s3_bucket_terraform_state_id}" \
                -backend-config="key=${TF_VAR_tfstate_setup_key:?}"
+
 s3_bucket_audit_log_id=$(terraform output s3_bucket_audit_log_id)
+echo "s3_bucket_audit_log_id: ${s3_bucket_audit_log_id}"
+
 s3_bucket_audit_log_bucket_domain_name=$(terraform output s3_bucket_audit_log_bucket_domain_name)
+echo "s3_bucket_audit_log_bucket_domain_name: ${s3_bucket_audit_log_bucket_domain_name}"
+
 s3_bucket_api_log_arn=$(terraform output s3_bucket_api_log_arn)
+echo "s3_bucket_api_log_arn: ${s3_bucket_api_log_arn}"
 
 cd - || exit 99
 
-## web
+##
+echo "START: web..."
+##
 
 ./bin/exec_codebuild.sh "${codebuild_destroy_web_name}" "${codecommit_web_branch:?}"
 
@@ -55,7 +73,9 @@ terraform destroy -auto-approve
 
 cd - || exit 99
 
-## base
+##
+echo "START: base..."
+##
 
 cd environments/service/base/after_api/ || exit 99
 
@@ -66,7 +86,9 @@ terraform destroy -auto-approve
 
 cd - || exit 99
 
-## api
+##
+echo "START: api..."
+##
 
 ./bin/mapping_logs_delete_firehose.sh "${cloudformation_api_stack}"
 ./bin/exec_codebuild.sh "${codebuild_destroy_api_name" "$codecommit_api_branch}"
@@ -80,7 +102,9 @@ terraform destroy -auto-approve
 
 cd - || exit 99
 
-## base
+##
+echo "START: base..."
+##
 
 cd environments/service/base/pre/ || exit 99
 
