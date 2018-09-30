9d13ee8f2e9eca79fc1931def1ceb6aaca570063
Makoto Nihei <nihemak@gmail.com>
2018-09-30 14:22:02 +0900

    Move build commands in buildspec_setup to bin

---
+++ bin/create_setup_staging_production.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+##
+echo "START: setup..."
+##
+
+cd environments/setup/staging_production/ || exit 99
+
+terraform init -backend-config="bucket=${TF_VAR_s3_bucket_terraform_state_id:?}" \
+               -backend-config="key=${TF_VAR_tfstate_setup_key:?}"
+terraform plan
+terraform apply -auto-approve
+
+aws codepipeline start-pipeline-execution --name "$(terraform output codepipeline_service_name)"
+
+cd - || exit 99
